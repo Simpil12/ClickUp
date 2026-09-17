1765bc57a048da308e13e3f529a2c23826fd2a43
Simpil <135355827+Simpil12@users.noreply.github.com>
2026-09-17 14:16:18 +1000

    Add files via upload
    - Added number formatting
    - Added Light and Dark mode toggle
    - Improved UI

--- main.js
+++ main.js
@@ -1,17 +1,25 @@
+var rawSave = localStorage.getItem("clicksUpSave");
+var saveGame = rawSave !== null ? JSON.parse(rawSave) : null;
+
 var gameData = {
     clicks: 0,
     clicksPerClick: 1,
-    clicksPerClickCost: 10
+    clicksPerClickCost: 10,
+    lastTick: Date.now()
 }
 
-var savegame = JSON.parse(localStorage.getItem("clicksUpSave"))
-if (savegame !== null) {
-    gameData = savegame
+function update(id, content) {
+  document.getElementById(id).innerHTML = content;
+}
+
+function updateUI() {
+    update("clicksUp", format(gameData.clicks, "scientific") + " Times Clicked");
+    update("perClickUpgrade", "Upgrade Clicks Per Click (Currently Level " + format(gameData.clicksPerClick, "scientific") + ") Cost: " + format(gameData.clicksPerClickCost, "scientific") + " Clicks");
 }
 
 function clicksUp() {
     gameData.clicks += gameData.clicksPerClick
-    document.getElementById("clicksUp").innerHTML = gameData.clicks + " Times Clicked"
+    updateUI()
 }
 
 function buyClicksPerClick() {
@@ -20,27 +28,41 @@ function buyClicksPerClick() {
         gameData.clicksPerClick += 1
         gameData.clicksPerClickCost *= 2
         updateUI()
-        localStorage.setItem("clicksUpSave", JSON.stringify(gameData))
     }
 }
 
-function updateUI() {
-    var clickDisplay = document.getElementById("clicksUp");
-    var perClickUpgrade = document.getElementById("perClickUpgrade");
-    if (clickDisplay) {
-        clickDisplay.innerHTML = gameData.clicks + " Times Clicked";
-    }
-    if (perClickUpgrade) {
-        perClickUpgrade.innerHTML = "Upgrade Clicks (Currently Level " + gameData.clicksPerClick + ") Cost: " + gameData.clicksPerClickCost + " Clicks";
-    }
+function tab(tab) {
+    document.getElementById("clickMenu").style.display = "none";
+    document.getElementById("upgradeMenu").style.display = "none";
+    document.getElementById(tab).style.display = "inline-block";
 }
+
+tab("clickMenu")
 
 var mainGameLoop = window.setInterval(function() {
-    clicksUp()
+    diff = Date.now() - gameData.lastTick;
+    gameData.lastTick = Date.now();
+    gameData.clicks += gameData.clicksPerClick * (diff / 1000);
+    updateUI()
 }, 1000)
 
 var saveGameLoop = window.setInterval(function() {
     localStorage.setItem("clicksUpSave", JSON.stringify(gameData))
 }, 15000)
+
+function format(number, type) {
+	let exponent = Math.floor(Math.log10(number))
+	let mantissa = number / Math.pow(10, exponent)
+	if (exponent < 3) return number.toFixed(1)
+	if (type == "scientific") return mantissa.toFixed(2) + "e" + exponent
+	if (type == "engineering") return (Math.pow(10, exponent % 3) * mantissa).toFixed(2) + "e" + (Math.floor(exponent / 3) * 3)
+}
+
+if (saveGame !== null) {
+    if (typeof saveGame.clicks !== "undefined") gameData.clicks = saveGame.clicks;
+    if (typeof saveGame.clicksPerClick !== "undefined") gameData.clicksPerClick = saveGame.clicksPerClick;
+    if (typeof saveGame.clicksPerClickCost !== "undefined") gameData.clicksPerClickCost = saveGame.clicksPerClickCost;
+    if (typeof saveGame.lastTick !== "undefined") gameData.lastTick = saveGame.lastTick;
+}
 
 updateUI()
